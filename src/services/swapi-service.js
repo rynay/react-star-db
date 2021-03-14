90cf9acc171c24b69f1be53dd1bb83aa3c4528d7
Rynay <feirs911@gmail.com>
2021-03-14 15:25:30 +0300

    refactoring in swapi-service control class

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -9,12 +9,74 @@ export default class SwapiService {
       throw new Error(`Could not fetch ${url}` +
         `, received ${res.status}`)
     }
-    return this._transformPlanet(await res.json());
+    return await res.json();
   }
 
+  getAllPeople = async () => {
+    const res = await this.getResource(`/people/`);
+    return res.results.map(item => this._transformPerson(item));
+  }
+  getAllPlanets = async () => {
+    const res = await this.getResource(`/planets/`);
+    return res.results.map(item => this._transformPlanet(item));
+  }
+  getAllStarships = async () => {
+    const res = await this.getResource(`/starships/`);
+    return res.results.map(item => this._transformStarship(item));
+  }
+
+  
+  getPerson = async (id) => {
+    const person = await this.getResource(`/people/${id}/`);
+    return this._transformPerson(person);
+  }
+  getPlanet = async (id) => {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+  }
+  getStarship = async (id) => {
+    const starship = await this.getResource(`/starships/${id}/`);
+    return this._transformStarship(starship);
+  }
+
+
+  _transformPerson(person){
+    console.log(person)
+    const id = person.url.match(/\/([0-9])\/$/)
+    const { 
+      name, 
+      gender, 
+      eye_color: eyeColor, 
+      birth_year: birthYear } = person;
+    return {
+      name,
+      gender,
+      eyeColor,
+      birthYear,
+      id,
+    }
+  }
+  _transformStarship(starship){
+    console.log(starship)
+    const id = starship.url.match(/\/([0-9])\/$/)
+    const { 
+      name, 
+      cost_in_credits: costInCredits,
+     } = starship;
+    return {
+      name,
+      costInCredits,
+      id,
+    }
+  }
   _transformPlanet(planet){
+    console.log(planet)
     const id = planet.url.match(/\/([0-9])\/$/)
-    const { name, population, rotation_period: rotationPeriod, diameter } = planet;
+    const { 
+      name, 
+      population, 
+      rotation_period: rotationPeriod, 
+      diameter } = planet;
     return {
       name,
       population,
@@ -22,32 +84,5 @@ export default class SwapiService {
       diameter,
       id,
     }
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planets/${id}/`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starships/${id}/`);
   }
 }
