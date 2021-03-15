d2e4600da6ff92dd628e275d757492b6b8a56c93
Rynay <feirs911@gmail.com>
2021-03-15 19:12:43 +0300

    fixing warnings

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,37 +1,35 @@
 import './random-planet.css';
 import { useEffect, useState } from 'react';
 import ItemDetails from '../item-details';
-import SwapiService from '../../services/swapi-service'
+import SwapiService from '../../services/swapi-service';
 
 const RandomPlanet = (props) => {
-  const swapiService = new SwapiService();
-  const [ planet, setPlanet ] = useState({
-    name: "Coruscant",
-    rotation_period: "24",
-    diameter: "12240",
-    population: "1000000000000",
+  const [planet, setPlanet] = useState({
+    name: 'Coruscant',
+    rotation_period: '24',
+    diameter: '12240',
+    population: '1000000000000',
     id: 9,
   });
 
-  useEffect(()=>{
+  useEffect(() => {
+    const swapiService = new SwapiService();
     const interval = setInterval(() => {
       const randomPlanetNumber = Math.floor(Math.random() * 18) + 2;
-      swapiService.getItem(randomPlanetNumber, props.category)
-      .then((item) => {
+      swapiService.getItem(randomPlanetNumber, props.category).then((item) => {
         setPlanet(item);
-      })
-    }, 3500)
+      });
+    }, 3500);
 
-    return () => { 
-      clearInterval(interval)
-    }
-  }, [planet])
+    return () => {
+      clearInterval(interval);
+    };
+  }, [props.category]);
 
   return (
     <div id="random-planet" className="random-planet jumbotron rounded card">
-      <ItemDetails category="planets" item={planet}/>
+      <ItemDetails category="planets" item={planet} />
     </div>
   );
-  
-}
+};
 export default RandomPlanet;
