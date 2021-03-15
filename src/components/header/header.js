8335473be3ecbef9abe68bd6956729acf4c1ac0d
Rynay <feirs911@gmail.com>
2021-03-15 19:19:53 +0300

    fixing warnings

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,19 +6,17 @@ const Header = () => {
   return (
     <div className="header d-flex">
       <h3>
-        <a href="#">
-          Star DB
-        </a>
+        <a href="#1">Star DB</a>
       </h3>
       <ul className="d-flex">
         <li>
-          <a href="#">People</a>
+          <a href="#1">People</a>
         </li>
         <li>
-          <a href="#">Planets</a>
+          <a href="#1">Planets</a>
         </li>
         <li>
-          <a href="#">Starships</a>
+          <a href="#1">Starships</a>
         </li>
       </ul>
     </div>
