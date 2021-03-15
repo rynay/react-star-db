e22fd4b4d85a88d9b97c29ebd9e268024e8f58e2
Rynay <feirs911@gmail.com>
2021-03-15 19:13:39 +0300

    fixing warnings

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,35 +1,40 @@
 import { useState, useEffect } from 'react';
-import Spinner from '../spinner'
+import Spinner from '../spinner';
 import './item-list.css';
-import SwapiService from '../../services/swapi-service'
+import SwapiService from '../../services/swapi-service';
 
 const ItemList = (props) => {
-  const [ isLoaded, setIsLoaded ] = useState(false);
-  const [ items, setItems ] = useState(null);
-
-  const swapiService = new SwapiService();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState(null);
 
   useEffect(() => {
-    swapiService.getAll(props.category)
-    .then((data) =>{
+    const swapiService = new SwapiService();
+    swapiService.getAll(props.category).then((data) => {
       setItems(data);
       setIsLoaded(true);
-    })
-  }, [])
-  
-    return (
-      <ul className="item-list list-group">
-      {!isLoaded ? <Spinner /> : items.map((item) => {
-        return (
-        <li  
-          key={item.id} 
-          className="list-group-item"
-          onClick={() => props.handleClick(item.id, props.category)}>
-          {item.name} ({ item.diameter || item.cost_in_credits || item.birth_year})
-        </li>)
-      })}
-      </ul>
-    );
-}
+    });
+  }, [props.category]);
+
+  return (
+    <ul className="item-list list-group">
+      {!isLoaded ? (
+        <Spinner />
+      ) : (
+        items.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className="list-group-item"
+              onClick={() => props.handleClick(item.id, props.category)}
+            >
+              {item.name} (
+              {item.diameter || item.cost_in_credits || item.birth_year})
+            </li>
+          );
+        })
+      )}
+    </ul>
+  );
+};
 
 export default ItemList;
