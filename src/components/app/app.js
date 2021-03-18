4df77b30957c9a4b284b9ec7ccdf4063ff473c50
Rynay <feirs911@gmail.com>
2021-03-18 22:28:57 +0300

    spinner added while item details is loading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,57 +6,11 @@ import RandomPlanet from '../random-planet';
 import ItemList from '../item-list';
 import ItemDetails from '../item-details';
 import Row from '../row';
-import SwapiService from '../../services/swapi-service';
 import { Switch, Route } from 'react-router-dom';
 
 import './app.css';
 
 const App = () => {
-  // const [isPersonLoaded, setIsPersonLoaded] = useState(true);
-  // const [isPlanetLoaded, setIsPlanetLoaded] = useState(true);
-  // const [isStarshipLoaded, setIsStarshipLoaded] = useState(true);
-  // const [person, setPerson] = useState();
-  // const [planet, setPlanet] = useState();
-  // const [starship, setStarship] = useState();
-  
-  // const swapiService = new SwapiService();
-  
-
-  // const getItem = (id, category) => {
-  //   switch (category) {
-  //     case 'people':
-  //       setIsPersonLoaded(false);
-  //       break;
-  //     case 'planets':
-  //       setIsPlanetLoaded(false);
-  //       break;
-  //     case 'starships':
-  //       setIsStarshipLoaded(false);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  //   swapiService.getItem(id, category).then((data) => {
-  //     switch (category) {
-  //       case 'people':
-  //         changeStates(data, setPerson, setIsPersonLoaded);
-  //         break;
-  //       case 'planets':
-  //         changeStates(data, setPlanet, setIsPlanetLoaded);
-  //         break;
-  //       case 'starships':
-  //         changeStates(data, setStarship, setIsStarshipLoaded);
-  //         break;
-  //       default:
-  //         break;
-  //     }
-  //   });
-  // };
-  // const changeStates = (data, func, loaded) => {
-  //   func({ ...data });
-  //   loaded(true);
-  // };
-
   const [ itemId, setItemId ] = useState(1);
 
   const changeItemId = (id) => {
@@ -85,7 +39,7 @@ const App = () => {
             />
           </Route>
 
-          <Route path="/starships/">
+          <Route path="/starships/:id?">
             <Row
               left={<ItemList category="starships" handleClick={changeItemId} />}
               right={<ItemDetails category="starships" itemId={itemId} />}
--- src/components/hoc/withData.js
+++ src/components/hoc/withData.js
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router';
 import SwapiService from '../../services/swapi-service';
+import Spinner from '../spinner';
 
 export const withData = (WrappedComponent) => (props) => {
   const [ item, setItem ] = useState(null);
+  const [ isLoaded, setIsLoaded ] = useState(false);
   const match = useRouteMatch();
 
-  console.log(match)
-
   useEffect(() => {
+    setIsLoaded(false);
     const swapiService = new SwapiService();
     swapiService.getItem(match.params.id || props.itemId, props.category).then((data) => {
       setItem(data);
+      setIsLoaded(true);
     });
   }, [props.category, props.itemId, match.params.id]);
 
-  return <WrappedComponent item={item} {...props} />;
+  return (
+    isLoaded ? <WrappedComponent item={item} {...props} /> : <Spinner />
+  )
 };
