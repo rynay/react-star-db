6ef568c256ed5a487a63f4d3b04d482a77e5de1d
Rynay <feirs911@gmail.com>
2021-03-18 23:06:07 +0300

    found and fixed bug with problem displaying items before reloads the page

--- src/components/hoc/withData.js
+++ src/components/hoc/withData.js
@@ -15,7 +15,7 @@ export const withData = (WrappedComponent) => (props) => {
   useEffect(() => {
     setIsLoaded(false);
     const swapiService = new SwapiService();
-    swapiService.getItem(match.params.id ? props.itemId : 5, props.category).then((data) => {
+    swapiService.getItem(match.params.id || props.itemId || 5, props.category).then((data) => {
       setItem(data);
       setIsLoaded(true);
     });
