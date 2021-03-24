19993ce15b7a12d0cf55638edd2f31404483a3c8
Rynay <feirs911@gmail.com>
2021-03-24 20:18:28 +0300

    random planet loading

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,39 +10,57 @@ import { Switch, Route } from 'react-router-dom';
 import './app.css';
 
 const App = () => {
-  const [ itemId, setItemId ] = useState(5);
+  const [itemId, setItemId] = useState(5);
 
   const changeItemId = (id) => {
     setItemId(id);
-  }
+  };
 
   return (
     <div className="container">
       <Header />
       {/* <RandomPlanet category="planets" /> */}
 
-        <Switch>
-          <Route path="/people/:id?">
-            <Row
-              left={<ItemList category="people" handleClick={changeItemId} />}
-              right={<ItemDetails category="people" changeItemId={changeItemId} itemId={itemId} />}
-            />
-          </Route>
+      <Switch>
+        <Route path="/people/:id?">
+          <Row
+            left={<ItemList category="people" handleClick={changeItemId} />}
+            right={
+              <ItemDetails
+                category="people"
+                changeItemId={changeItemId}
+                itemId={itemId}
+              />
+            }
+          />
+        </Route>
 
-          <Route path="/planets/:id?">
-            <Row
-              left={<ItemList category="planets" handleClick={changeItemId} />}
-              right={<ItemDetails category="planets" changeItemId={changeItemId} itemId={itemId} />}
-            />
-          </Route>
+        <Route path="/planets/:id?">
+          <Row
+            left={<ItemList category="planets" handleClick={changeItemId} />}
+            right={
+              <ItemDetails
+                category="planets"
+                changeItemId={changeItemId}
+                itemId={itemId}
+              />
+            }
+          />
+        </Route>
 
-          <Route path="/starships/:id?">
-            <Row
-              left={<ItemList category="starships" handleClick={changeItemId} />}
-              right={<ItemDetails category="starships" changeItemId={changeItemId} itemId={itemId} />}
-            />
-          </Route>
-        </Switch>
+        <Route path="/starships/:id?">
+          <Row
+            left={<ItemList category="starships" handleClick={changeItemId} />}
+            right={
+              <ItemDetails
+                category="starships"
+                changeItemId={changeItemId}
+                itemId={itemId}
+              />
+            }
+          />
+        </Route>
+      </Switch>
     </div>
   );
 };
