be2717b93ccb4823ec090a252ec1ba5ac28b7d0a
Rynay <feirs911@gmail.com>
2021-03-24 20:08:30 +0300

    moved business logic from item-list to item-list-container

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,29 +1,16 @@
-import { useState, useEffect } from 'react';
 import Spinner from '../spinner';
 import './item-list.css';
-import SwapiService from '../../services/swapi-service';
 import { Link } from 'react-router-dom';
 
 const ItemList = (props) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState(null);
-
-  useEffect(() => {
-    setIsLoaded(false);
-    const swapiService = new SwapiService();
-    swapiService.getAll(props.category).then((data) => {
-      setItems(data);
-      setIsLoaded(true);
-    });
-  }, [props.category]);
-
+  const { isLoaded, items } = props;
   return (
     <ul className="item-list list-group">
       {!isLoaded ? (
         <Spinner />
-        ) : (
-          items.map((item) => {
-            return (
+      ) : (
+        items.map((item) => {
+          return (
             <Link key={item.id} to={`/${props.category}/${item.id}`}>
               <li
                 className="list-group-item"
@@ -33,7 +20,6 @@ const ItemList = (props) => {
                 {item.diameter || item.cost_in_credits || item.birth_year})
               </li>
             </Link>
-            
           );
         })
       )}
