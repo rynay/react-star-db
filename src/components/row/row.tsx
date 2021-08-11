3667b8f1c1e5e9a4073b1bdd4b35ec84172684bd
Rynay <feirs911@gmail.com>
2021-08-11 11:40:46 +0300

    comps/row => ts

---
+++ src/components/row/row.tsx
@@ -0,0 +1,22 @@
+import ErrorBoundary from '../error-boundary'
+import React, { ReactElement } from 'react'
+
+type Props = {
+  left: ReactElement
+  right: ReactElement
+}
+
+const Row = ({ left, right }: Props) => {
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <ErrorBoundary>{left}</ErrorBoundary>
+      </div>
+      <div className="col-md-6">
+        <ErrorBoundary>{right}</ErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default Row
