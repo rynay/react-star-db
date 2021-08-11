4348cb41e8bb8086a1b32e58532d73c36cb2de6f
Rynay <feirs911@gmail.com>
2021-08-11 11:54:04 +0300

    services => ts

---
+++ src/services/swapi-service.ts
@@ -0,0 +1,78 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.dev/api'
+  _imageBase = 'https://starwars-visualguide.com/assets/img'
+
+  getResource = async (url: string) => {
+    try {
+      const res = await fetch(`${this._apiBase}${url}`)
+      if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, received ${res.status}`)
+      }
+      return await res.json()
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  getAll = async (category: string) => {
+    const data = await this.getResource(`/${category}/`)
+    const results: (TPlanet | TPerson | TStarship)[] = data.results
+    return results.map((item) => this._transform(item, category))
+  }
+
+  getItem = async (id: TItem['id'], category: string) => {
+    const item = await this.getResource(`/${category}/${id}/`)
+    return this._transform(item, category)
+  }
+
+  getImage = async (id: TItem['id'], category: string) => {
+    if (!id || !category) {
+      return 'https://grist.org/wp-content/uploads/2012/10/question-mark-earth-470.jpg'
+    }
+    if (category === 'people') {
+      category = 'characters'
+    }
+
+    const res = await fetch(
+      `https://starwars-visualguide.com/assets/img/${category}/${id}.jpg`
+    )
+    if (res.ok)
+      return `https://starwars-visualguide.com/assets/img/${category}/${id}.jpg`
+    if (!res.ok)
+      return 'https://grist.org/wp-content/uploads/2012/10/question-mark-earth-470.jpg'
+  }
+
+  _transform(item: TPerson | TPlanet | TStarship, category: string) {
+    switch (category) {
+      case 'people':
+        if (!(item.category === 'people')) return
+        return {
+          name: item.name,
+          gender: item.gender,
+          eye_color: item.eye_color,
+          birth_year: item.birth_year,
+          id: item.url.match(/\/([0-9]+)\/$/)![1],
+        }
+      case 'planets':
+        if (!(item.category === 'planets')) return
+        return {
+          name: item.name,
+          population: item.population,
+          rotation_period: item.rotation_period,
+          diameter: item.diameter,
+          id: item.url.match(/\/([0-9]+)\/$/)![1],
+        }
+      case 'starships':
+        if (!(item.category === 'starships')) return
+        return {
+          name: item.name,
+          cost_in_credits: item.cost_in_credits,
+          max_atmosphering_speed: item.max_atmosphering_speed,
+          passengers: item.passengers,
+          id: item.url.match(/\/([0-9]+)\/$/)![1],
+        }
+      default:
+        return item
+    }
+  }
+}
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,7 +1,26 @@
 type TItem = {
   id: number
   name: string
-  diameter: number
-  cost_in_credits: number
+  url: string
+}
+
+type TPerson = TItem & {
+  category: 'people'
   birth_year: string
+  gender: string
+  eye_color: string
+}
+
+type TPlanet = TItem & {
+  category: 'planets'
+  population: number
+  rotation_period: number
+  diameter: number
+}
+
+type TStarship = TItem & {
+  category: 'starships'
+  cost_in_credits: number
+  max_atmosphering_speed: number
+  passengers: number
 }
